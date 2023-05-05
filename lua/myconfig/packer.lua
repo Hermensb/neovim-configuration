87d7f47bd25498fdf4b7a8ad2447ef3e75fad573
Brad Hermens <brad.hermens@gmail.com>
2023-05-05 06:37:25 -0700

    fix: add cmp requirements

--- lua/myconfig/packer.lua
+++ lua/myconfig/packer.lua
@@ -16,8 +16,11 @@ return require('packer').startup(function(use)
 		"neovim/nvim-lspconfig",
 	}
 	use {
-		'hrsh7th/nvim-cmp',
 		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-cmdline',
+		'hrsh7th/nvim-cmp',
 		'L3MON4D3/LuaSnip',
 	}
 
